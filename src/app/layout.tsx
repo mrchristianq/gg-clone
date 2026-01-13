449240ca1b885f10f65ffe82cf51da6625f78433
mrchristianq <80832270+mrchristianq@users.noreply.github.com>
2026-01-13 18:53:48 -0500

    Add Home tab layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,12 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
